f837cd1f45a60b377964a854b81a061acad87708
Zazaborisovi <geozedx@mail.ru>
2023-10-05 20:38:49 +0400

    first commit

---
+++ day 3/homework4.py
@@ -0,0 +1,25 @@
+Mom = 42
+Dad = 48
+Sister = 19
+Me = 16
+
+mom = int(input('moms age 42: '))
+if mom == 42:
+    print(Mom + 20)
+else:
+    print('incorrect')
+dad = int(input('dads age 48 : '))
+if dad == 48:
+    print(Dad + 20)
+else:
+    print('incorrect')
+sister = int(input('sisters age 19 : '))
+if sister == 19:
+    print(Sister + 20)
+else:
+    print('incorrect')
+me = int(input('my age 16 : '))
+if me == 16:
+    print(Me + 20)
+else:
+    print('incorrect')
