70db266edadfa5af5437503dad90441a86a5ef67
Zazaborisovi <geozedx@mail.ru>
2023-10-12 16:04:56 +0400

    first commit

---
+++ day 5/homework.py
@@ -0,0 +1,22 @@
+#numbers loop
+print('NUMBER LOOP')
+a = 1
+b = 2
+while a and b <= 30:
+    print(str(a)+(' kenti'))
+    a = a + 2
+    print(str(b)+(' luwi'))
+    b = b + 2
+
+
+#day loop
+print('DAY LOOP')
+day = 12
+while day <= 12:
+    print('wake up')
+    print('workout')
+    print('eat')
+    print('study')
+    print('workout')
+    print('sleep')
+    day = day + 1
