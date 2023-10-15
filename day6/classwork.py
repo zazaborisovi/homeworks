c461d380f53128f135944dfed7fc6cdf0887d60f
Zazaborisovi <geozedx@mail.ru>
2023-10-15 15:53:52 +0400

    first commit

---
+++ day6/classwork.py
@@ -0,0 +1,9 @@
+ticket = 25
+adult = 10
+child = 3
+while True:
+    adult = adult * 25
+    print(str(adult) + ('$ - for 10 adults'))
+    child = child * 0
+    print (str(child) + ('$ - for childrens'))
+    break
