0c2ee4d9b9ea9bf3d26f1ba88b8a8663a9e8a682
Zazaborisovi <geozedx@mail.ru>
2023-11-02 21:16:05 +0400

    first commit

--- day 11/day 11 hw.py
+++ day 11/day 11 hw.py
@@ -1,8 +1,11 @@
-print ('write how big your triangle sides you want to be: ')
-side1 = int(input('side 1: '))
-side2 = int(input('side 2: '))
-side3 = int(input('side 3: '))
-if side1 and side2 > side3 or side2 and side3 > side1 or side1 and side3 > side2:
-    print('your triangle will be created')
-else:
-    print('your triangle cant be created by that sizes you have writen')
+def triangle():
+    print ('write how big your triangle sides you want to be: ')
+    side1 = int(input('side 1: '))
+    side2 = int(input('side 2: '))
+    side3 = int(input('side 3: '))
+    if side1 and side2 > side3 or side2 and side3 > side1 or side1 and side3 > side2:
+        print('your triangle will be created')
+    else:
+        print('your triangle cant be created by that sizes you have writen')
+
+triangle()
