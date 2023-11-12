edb4d55bc06214e674f062a6c3cc0d9cbafb09fb
Zazaborisovi <geozedx@mail.ru>
2023-11-12 16:18:54 +0400

    first commit

---
+++ day 12/day 12CW.py
@@ -0,0 +1,7 @@
+def person(line , name , line2 , age):
+    print(line + name + line2 , age)
+
+person('your name is: ' , 'zaza ' , 'your age is: ' , '16')
+person('your name is: ' , 'ana ' , 'your age is: ' , '19')
+person('your name is: ' , 'lela ' , 'your age is: ' , '42')
+person('your name is: ' , 'giga ' , 'your age is: ' , '48')
