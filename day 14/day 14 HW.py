4416e847cfc82dfb176a5fff92aeb3057458a1c9
Zazaborisovi <geozedx@mail.ru>
2023-11-16 17:34:00 +0400

    first commit

---
+++ day 14/day 14 HW.py
@@ -0,0 +1,55 @@
+#1 function used to display text in python is:
+print('function print')
+#2 in python integer is the data type that stores whole number
+my_int = 20 #whole number
+#3 in python text is stored in string data type
+my_str = 'string'
+#4
+print(10 + 5 * 2) #20
+#5
+value = 42
+#6 in python process of identifying and fixing errors in program is called debugging
+#7 in python commonly used case is snake case
+my_case = 'snake_case'
+#8 commenting in python helps you and your team to read code correctly
+#9
+input('taking your input: ')
+#11
+print(type(my_int))
+#12 there are lots of ways to convert integer into string but the easiest one is
+print(type(str(my_int)))
+'''
+#13 term of converting one data type to another is called converting 
+#14 == operator checks if the two values are equal
+#15 in True and False the operation will end up false but only if its True and True it will endup true otherwise false
+#16 (5 > 3) and (10 < 20) both are true so the operation will end up true
+'''
+if (5 > 3) and (10 < 20):
+    print('True')
+'''
+17 if statment is used to make decisions and execute different code blocks based on conditions
+18 while loop is used to iterate over a sequence
+19 for loop is used to repeat blocks as long as condition is true
+20 range() function returns a sequence of numbers
+21 if itself starts if statment
+22 if if or else statment doesnt work else statment executes
+23 in python list data is used to store collection of items
+24 in python first element in list is on 0 index
+25 to access 3rd element stands on 2nd index
+26 slicing allows to slice chosen text
+'''
+27
+my_list = [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9]
+print(my_list[2:6])
+'''
+28 steps in slicing tells the machine how much to step
+29 reusable code block is called def/function
+30 in python values that are passed to function are called parameters
+31
+'''
+print(my_str.upper())
+#32
+my_list.append(10)
+print(my_list)
+#33 def defines new function
+#34 return is keyword that returns a value from function
