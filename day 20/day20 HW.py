6912d864c0faed5cb9abe41a75f5baef1570e179
Zazaborisovi <geozedx@mail.ru>
2023-12-03 21:11:22 +0400

    first commit

--- day 20/day20 HW.py
+++ day 20/day20 HW.py
@@ -1,6 +1,6 @@
 #returning strings
 def greet(name):
-    'Hello, ' + name + ' how are you doing today'
+    return 'Hello, ' + name + ' how are you doing today'
 
 #sum of arrays
 def sum_array(a):
