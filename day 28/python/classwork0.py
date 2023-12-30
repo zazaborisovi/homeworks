cbb31b19a6068537e437dd6683d09d06c112d545
Zazaborisovi <geozedx@mail.ru>
2023-12-30 22:48:01 +0400

    first commit

--- day 28/python/classwork0.py
+++ day 28/python/classwork0.py
@@ -1,8 +1,10 @@
 x = float(input("first number: "))
 y = float(input("second number: "))
 
+#giving user of code choise for what kind of mathematical decision machine needs to make
 option = int(input("choose one option ( 1 for add , 2 for substract , 3 for multiply , 4 for divide)"))
 
+#making if statment so if user of code chooses any of the options they will get answer they need
 if option == 1:
     add = print(float(x + y))
 elif option == 2:
